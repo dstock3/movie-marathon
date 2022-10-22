51c3d81c2832d232c42935f8fd85c809173b991d
David Stockdale <dstock3@gmail.com>
2022-10-22 15:55:01 -0500

    Preparing to build a function called showDate which will pull up a modal with more details about a particular date.

--- src/components/calendar/CalendarGrid.tsx
+++ src/components/calendar/CalendarGrid.tsx
@@ -39,6 +39,11 @@ const CalendarGrid = (props: GridProps) => {
         }
     }, [props.monthRange])
 
+    const showDate = (id: number) => {
+        
+
+    }
+
     useEffect(()=> {
         let dates = []
 
@@ -67,142 +72,142 @@ const CalendarGrid = (props: GridProps) => {
     return (
         <div className="calendar-grid">
             <div className="row" id="week-one">
-                <div className="square" id="1">
+                <div className="square" id="1" onClick={()=> showDate(1)}>
 
                 </div>
-                <div className="square" id="2">
+                <div className="square" id="2" onClick={()=> showDate(2)}>
 
                 </div>
-                <div className="square" id="3">
+                <div className="square" id="3" onClick={()=> showDate(3)}>
 
                 </div>
-                <div className="square" id="4">
+                <div className="square" id="4" onClick={()=> showDate(4)}>
 
                 </div>
-                <div className="square" id="5">
+                <div className="square" id="5" onClick={()=> showDate(5)}>
 
                 </div>
-                <div className="square" id="6">
+                <div className="square" id="6" onClick={()=> showDate(6)}>
 
                 </div>
-                <div className="square" id="7">
+                <div className="square" id="7" onClick={()=> showDate(7)}>
 
                 </div>
             </div>
             <div className="row" id="week-two">
-                <div className="square" id="8">
+                <div className="square" id="8" onClick={()=> showDate(8)}>
 
                 </div>
-                <div className="square" id="9">
+                <div className="square" id="9" onClick={()=> showDate(9)}>
 
                 </div>
-                <div className="square" id="10">
+                <div className="square" id="10" onClick={()=> showDate(10)}>
 
                 </div>
-                <div className="square" id="11">
+                <div className="square" id="11" onClick={()=> showDate(11)}>
 
                 </div>
-                <div className="square" id="12">
+                <div className="square" id="12" onClick={()=> showDate(12)}>
 
                 </div>
-                <div className="square" id="13">
+                <div className="square" id="13" onClick={()=> showDate(13)}>
 
                 </div>
-                <div className="square" id="14">
+                <div className="square" id="14" onClick={()=> showDate(14)}>
 
                 </div>
             </div>
             <div className="row" id="week-three">
-                <div className="square" id="15">
+                <div className="square" id="15" onClick={()=> showDate(15)}>
 
                 </div>
-                <div className="square" id="16">
+                <div className="square" id="16" onClick={()=> showDate(16)}>
 
                 </div>
-                <div className="square" id="17">
+                <div className="square" id="17" onClick={()=> showDate(17)}>
 
                 </div>
-                <div className="square" id="18">
+                <div className="square" id="18" onClick={()=> showDate(18)}>
 
                 </div>
-                <div className="square" id="19">
+                <div className="square" id="19" onClick={()=> showDate(19)}>
 
                 </div>
-                <div className="square" id="20">
+                <div className="square" id="20" onClick={()=> showDate(20)}>
 
                 </div>
-                <div className="square" id="21">
+                <div className="square" id="21" onClick={()=> showDate(21)}>
 
                 </div>
             </div>
             <div className="row" id="week-four">
-                <div className="square" id="22">
+                <div className="square" id="22" onClick={()=> showDate(22)}>
 
                 </div>
-                <div className="square" id="23">
+                <div className="square" id="23" onClick={()=> showDate(23)}>
 
                 </div>
-                <div className="square" id="24">
+                <div className="square" id="24" onClick={()=> showDate(24)}>
 
                 </div>
-                <div className="square" id="25">
+                <div className="square" id="25" onClick={()=> showDate(25)}>
 
                 </div>
-                <div className="square" id="26">
+                <div className="square" id="26" onClick={()=> showDate(26)}>
 
                 </div>
-                <div className="square" id="27">
+                <div className="square" id="27" onClick={()=> showDate(27)}>
 
                 </div>
-                <div className="square" id="28">
+                <div className="square" id="28" onClick={()=> showDate(28)}>
 
                 </div>
             </div>
             <div className="row" id="week-five">
-                <div className="square" id="29">
+                <div className="square" id="29" onClick={()=> showDate(29)}>
 
                 </div>
-                <div className="square" id="30">
+                <div className="square" id="30" onClick={()=> showDate(30)}>
 
                 </div>
-                <div className="square" id="31">
+                <div className="square" id="31" onClick={()=> showDate(31)}>
 
                 </div>
-                <div className="square" id="32">
+                <div className="square" id="32" onClick={()=> showDate(32)}>
 
                 </div>
-                <div className="square" id="33">
+                <div className="square" id="33" onClick={()=> showDate(33)}>
 
                 </div>
-                <div className="square" id="34">
+                <div className="square" id="34" onClick={()=> showDate(34)}>
 
                 </div>
-                <div className="square" id="35">
+                <div className="square" id="35" onClick={()=> showDate(35)}>
 
                 </div>
             </div>
             {props.monthRange ?
                 props.monthRange.length > 30 && start > 5?
                     <div className="row" id="week-six">
-                        <div className="square" id="36">
+                        <div className="square" id="36" onClick={()=> showDate(36)}>
         
                         </div>
-                        <div className="square" id="37">
+                        <div className="square" id="37" onClick={()=> showDate(37)}>
         
                         </div>
-                        <div className="square" id="38">
+                        <div className="square" id="38" onClick={()=> showDate(38)}>
         
                         </div>
-                        <div className="square" id="39">
+                        <div className="square" id="39" onClick={()=> showDate(39)}>
         
                         </div>
-                        <div className="square" id="40">
+                        <div className="square" id="40" onClick={()=> showDate(40)}>
         
                         </div>
-                        <div className="square" id="41">
+                        <div className="square" id="41" onClick={()=> showDate(41)}>
         
                         </div>
-                        <div className="square" id="42">
+                        <div className="square" id="42" onClick={()=> showDate(42)}>
         
                         </div>
                     </div> : null 
