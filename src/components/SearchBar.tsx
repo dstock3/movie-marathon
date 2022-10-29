ff9006c436611b9ad27e90dfeb9b91021d7fa807
David Stockdale <dstock3@gmail.com>
2022-10-29 13:33:23 -0500

    Made stylistic adjustments to search icon.

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -21,8 +21,6 @@ const SearchBar = (props: SearchProps) => {
   
   useEffect(()=> {
     if (props.thisUser) {
-      //change search icon depending on user theme
-
       let icon = document.getElementById("search-path")
 
       let themes = Object.keys(theme)
@@ -38,7 +36,6 @@ const SearchBar = (props: SearchProps) => {
         }
       }
     }
-
   }, [props.thisUser])
 
   const handleSearch = () => {
@@ -65,10 +62,11 @@ const SearchBar = (props: SearchProps) => {
   return (
     <div className="search-container" style={searchStyle}>
         <div className="search-icon" onClick={handleSearch}>
-          <svg xmlns="http://www.w3.org/2000/svg" height="48" width="48"><path id="search-path" d="M39.8 41.95 26.65 28.8q-1.5 1.3-3.5 2.025-2 .725-4.25.725-5.4 0-9.15-3.75T6 18.75q0-5.3 3.75-9.05 3.75-3.75 9.1-3.75 5.3 0 9.025 3.75 3.725 3.75 3.725 9.05 0 2.15-.7 4.15-.7 2-2.1 3.75L42 39.75Zm-20.95-13.4q4.05 0 6.9-2.875Q28.6 22.8 28.6 18.75t-2.85-6.925Q22.9 8.95 18.85 8.95q-4.1 0-6.975 2.875T9 18.75q0 4.05 2.875 6.925t6.975 2.875Z"/>
+          <svg xmlns="http://www.w3.org/2000/svg" height="24" width="24">
+            <path id="search-path" d="m19.55 21.95-6.475-6.475q-.725.5-1.687.8-.963.3-2.088.3-2.95 0-5.012-2.063Q2.225 12.45 2.225 9.5q0-2.95 2.063-5.013Q6.35 2.425 9.3 2.425q2.95 0 5.012 2.062Q16.375 6.55 16.375 9.5q0 1.15-.3 2.087-.3.938-.8 1.638l6.5 6.525ZM9.3 13.425q1.65 0 2.788-1.138 1.137-1.137 1.137-2.787t-1.137-2.788Q10.95 5.575 9.3 5.575T6.513 6.712Q5.375 7.85 5.375 9.5t1.138 2.787Q7.65 13.425 9.3 13.425Z"/>
           </svg>
         </div>
-
+        
         <input className="search-input" value={searchInput} onChange={e => setSearchInput(e.target.value)}></input>
     </div>
   )
