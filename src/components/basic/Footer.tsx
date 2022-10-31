9da0780f89b50598f290d4a3b7ccaab769beac79
David Stockdale <dstock3@gmail.com>
2022-10-31 14:59:35 -0500

    Made stylistic adjustments to footer.

--- src/components/basic/Footer.tsx
+++ src/components/basic/Footer.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useContext } from 'react'
 import { ThemeContext } from '../context/ThemeContext'
+import '../../style/footer.css'
 
 type FooterProps = {
     thisStyle: React.CSSProperties,
