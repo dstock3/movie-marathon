07df1d800c29bc80efa3be704ee4d6f9b7aa6bb2
David Stockdale <dstock3@gmail.com>
2022-11-03 21:58:54 -0500

    Sorted post array within return statement in Feed component and removed unnecessary useEffect hook.

--- src/components/feed/Feed.tsx
+++ src/components/feed/Feed.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { Users, ThisUser } from '../basic/Main'
+import uniqueid from 'uniqid'
 
 type FeedProps = {
   users: Users,
@@ -8,6 +9,7 @@ type FeedProps = {
 }
 
 type Post = {
+  id: string,
   handle: string, 
   content: string, 
   date: string
@@ -22,6 +24,7 @@ const Feed = (props: FeedProps) => {
       for (let x = 0; x < props.users[i].posts.length; x++) {
         let post = props.users[i].posts[x]
         postArray.push({
+          id: uniqueid(),
           handle: props.users[i].handle, 
           content: post.content, 
           date: post.date
@@ -31,20 +34,16 @@ const Feed = (props: FeedProps) => {
     setPosts(postArray)
   }, [props.users])
 
-  useEffect(()=> {
-    posts?.sort(function compare(a, b) {
-      let dateA = new Date(a.date);
-      let dateB = new Date(b.date);
-      return dateB.valueOf() - dateA.valueOf();
-    });
-    console.log(posts)
-  }, [posts])
-
   return (
     <div className="feed-container">
-      {posts?.map((post: Post, index)=> {
+      {posts?.sort(function compare(a, b) {
+        let dateA = new Date(a.date);
+        let dateB = new Date(b.date);
+        return dateB.valueOf() - dateA.valueOf();
+      })
+      .map((post: Post, index)=> {
         return(
-          <div key={index} className="post-container">
+          <div key={post.id} className="post-container">
             <div className="post-head">
               <div className="post-handle">{post.handle}</div>
               <div className="post-date">{post.date}</div>
