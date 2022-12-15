d228564bd4fec69557f747a46f06ac90959f9376
David Stockdale <dstock3@gmail.com>
2022-12-15 17:11:31 -0600

    fleshed out Friends component

--- src/components/feed/Friends.tsx
+++ src/components/feed/Friends.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import {useState, useEffect} from 'react'
 
 const Friends = () => {
+  const [friends, setFriends] = useState<Array<Object>>([])
+
+  useEffect(()=> {
+    //API call to get friends
+
+    //setFriends(friends) 
+  }, [])
+
   return (
     <div className="friends-container">
-        Friends
+      <h3>Friends</h3>
+      <div className="friends">
+        {friends && friends.map((friend: Object, index: number)=> {
+          return (
+            <div className="friend" key={index}></div>
+          )
+        })}
+      </div>
     </div>
   )
 }
