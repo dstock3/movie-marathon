d4c419b6c58768e9d2855f40c252fd01baa6b98d
David Stockdale <dstock3@gmail.com>
2022-12-17 13:50:12 -0600

    Adjusted style of Suggestions elements

--- src/components/feed/Suggestions.tsx
+++ src/components/feed/Suggestions.tsx
@@ -1,9 +1,14 @@
-import { useEffect, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import Loader from '../basic/Loader'
 import { SuggestionsProps } from '../../Types.types'
+import { ThemeContext } from '../context/ThemeContext'
 
 const Suggestions = (props: SuggestionsProps) => {
   const [suggestions, setSuggestions] = useState<Array<Object>>([])
+  const [suggestionContainerStyle, setSuggestionContainerStyle] = useState<React.CSSProperties | Object>({})
+  const [suggestionHeadStyle, setSuggestionHeadStyle] = useState<React.CSSProperties | Object>({})
+  const [suggestionStyle, setSuggestionStyle] = useState<React.CSSProperties | Object>({})
+  const theme = useContext(ThemeContext)
 
   useEffect(()=> {
     //API call to get suggestions
@@ -12,13 +17,29 @@ const Suggestions = (props: SuggestionsProps) => {
 
   }, [])
 
+  useEffect(()=> {
+    let themes = Object.keys(theme)
+
+    if (props.thisUser) {
+      for (let i = 0; i < themes.length; i++) {
+        if (themes[i] === props.thisUser.theme) {
+          
+          let thisTheme: any = theme[themes[i] as keyof Object]
+
+          setSuggestionContainerStyle({border: thisTheme.border})
+          setSuggestionHeadStyle({borderBottom: thisTheme.border})
+        }
+      }
+    }
+  }, [props.thisStyle])
+
   return (
-    <div className="suggestions-container">
-      <h3>Suggestions</h3>
-      <div className="suggestions">
+    <div className="suggestions-container" style={suggestionContainerStyle}>
+      <h3 className="suggestion-head" style={suggestionHeadStyle}>Suggestions</h3>
+      <div className="suggestions" >
         {suggestions.length > 0 ? suggestions.map((suggestion: Object, index: number)=> {
           return ( 
-            <div className="suggestion" key={index}>
+            <div className="suggestion" style={suggestionStyle} key={index}>
 
             </div>
           )
