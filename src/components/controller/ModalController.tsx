983027c6393a2b16f1214dece607703fd0e81151
David Stockdale <dstock3@gmail.com>
2022-12-16 21:31:22 -0600

    Converted ModalController to switch statement

--- src/components/controller/ModalController.tsx
+++ src/components/controller/ModalController.tsx
@@ -1,19 +1,19 @@
-import React from 'react'
 import ToggleSidebar from '../modals/ToggleSidebar'
 import Compose from '../modals/Compose'
 import { ModalControllerProps } from '../../Types.types'
 
 const ModalController = (props: ModalControllerProps) => {
-    if (!props.isExpanded)
-        return (
-            <ToggleSidebar thisStyle={props.thisStyle} thisUser={props.thisUser} isExpanded={props.isExpanded} setIsExpanded={props.setIsExpanded} />
-        )
-    else if (props.timeToPost)
-        return (
-            <Compose thisStyle={props.thisStyle} thisUser={props.thisUser} setTimeToPost={props.setTimeToPost} />
-        )
-    else
-        return null
+  switch (true) {
+    case !props.isExpanded:
+      return (
+        <ToggleSidebar thisStyle={props.thisStyle} thisUser={props.thisUser} isExpanded={props.isExpanded} setIsExpanded={props.setIsExpanded} />
+      );
+    case props.timeToPost:
+      return (
+        <Compose thisStyle={props.thisStyle} thisUser={props.thisUser} setTimeToPost={props.setTimeToPost} />
+      );
+    default:
+      return null;
+  }
 }
-
 export default ModalController
