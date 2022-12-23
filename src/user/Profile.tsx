9e5aadab4b9940f5e7de72ee8c971751d7fa8c6c
David Stockdale <dstock3@gmail.com>
2022-12-23 14:49:41 -0600

    added notes

--- src/user/Profile.tsx
+++ src/user/Profile.tsx
@@ -2,6 +2,7 @@ import { ProfileProps } from "../Types.types"
 import '../style/profile.css'
 
 export const Profile = (props: ProfileProps) => {
+  //enable user to edit their handle, profile description, email, password, and profile picture
   return (
     <div className="profile-container">
       profile
--- src/user/Settings.tsx
+++ src/user/Settings.tsx
@@ -2,6 +2,7 @@ import { SettingsProps } from "../Types.types"
 import '../style/settings.css'
 
 const Settings = (props: SettingsProps) => {
+  //enable user to edit settings such as theme, allowing the collection of metadata, and notifications
   return (
     <div className="settings-container">
       settings
