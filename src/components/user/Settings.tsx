fda5b17e72e48dd8c0d8a1f5adc73fe39678fbd3
David Stockdale <dstock3@gmail.com>
2022-12-26 06:54:38 -0600

    Added various settings container elements.

--- src/components/user/Settings.tsx
+++ src/components/user/Settings.tsx
@@ -39,36 +39,43 @@ const Settings = (props: SettingsProps) => {
         <fieldset>
           <legend>Settings</legend>
 
-          <label htmlFor="theme-select">Select a theme:</label>
-          <select id="theme-select" value={theme} onChange={(event) => setTheme(event.target.value)}>
-            <option value="light">Light</option>
-            <option value="dark">Dark</option>
-            <option value="midnight">Midnight</option>
-            <option value="mint">Mint</option>
-          </select>
-          
-          <label htmlFor="metadata-toggle">Allow collection of metadata:</label>
-          <input
-            type="checkbox"
-            id="metadata-toggle"
-            checked={collectMetadata}
-            onChange={(event) => setCollectMetadata(event.target.checked)}
-          />
+          <div className="settings-description">
+            <p>Update your settings.</p>
+          </div>
 
-        
-          <label htmlFor="email-notifications">Email notifications:</label>
-          <input
-            type="checkbox"
-            id="email-notifications"
-            checked={emailNotifications}
-            onChange={(event) => setEmailNotifications(event.target.checked)}
-          />
+          <div className="theme-settings">
+            <label htmlFor="theme-select">Select a theme:</label>
+            <select id="theme-select" value={theme} onChange={(event) => setTheme(event.target.value)}>
+              <option value="light">Light</option>
+              <option value="dark">Dark</option>
+              <option value="midnight">Midnight</option>
+              <option value="mint">Mint</option>
+            </select>
+          </div>
 
+          <div className="metadata-settings">
+            <label htmlFor="metadata-toggle">Allow collection of metadata:</label>
+            <input
+              type="checkbox"
+              id="metadata-toggle"
+              checked={collectMetadata}
+              onChange={(event) => setCollectMetadata(event.target.checked)}
+            />
+          </div>
+
+          <div className="email-settings">
+            <label htmlFor="email-notifications">Email notifications:</label>
+            <input
+              type="checkbox"
+              id="email-notifications"
+              checked={emailNotifications}
+              onChange={(event) => setEmailNotifications(event.target.checked)}
+            />
+          </div>
           <div className="button-container">
             <button className="settings-button" id="reset-button" type="reset">Reset Form</button>
             <button className="settings-button" id="update-button" type="submit">Update Settings</button>
           </div>
-          
         </fieldset>
       </form>
     </div>
